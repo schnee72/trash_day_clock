13b725e3c5659c61d214e6d9ae6b67d3eeb8642e
Dustin Schnee <schnee72@users.noreply.github.com>
2022-03-05 22:39:29 -0600

    more cleanup

--- trash_day_clock.py
+++ trash_day_clock.py
@@ -8,7 +8,7 @@ import sys
 import time
 
 us_holidays = holidays.US()
-trash_holidays = [ # these are the only holidays that affect trash pickup
+trash_holidays = [ # only holidays that affect trash pickup
     'New Year\'s Day',
     'New Year\'s Day (Observed)',
     'Memorial Day',
@@ -19,7 +19,6 @@ trash_holidays = [ # these are the only holidays that affect trash pickup
     'Christmas Day',
     'Christmas Day (Observed)'
 ]
-
 i2c = board.I2C()
 display = Seg7x4(i2c)
 BLUE_LED = 17
@@ -32,7 +31,7 @@ def is_holiday(date):
     return bool(set(trash_holidays) & set(us_holidays.get_list(date)))
 
 def is_recycle_week(now):
-    known_recycle_date = datetime(2022,2,24,0,0,0)
+    known_recycle_date = datetime(2022, 2, 24, 0, 0, 0)
     return ((now - known_recycle_date).days / 7) % 2 < 1
 
 def leds_on(is_recycle_week):
@@ -43,19 +42,20 @@ def leds_on(is_recycle_week):
 def process_leds(now):
     GPIO.output(BLUE_LED, False)
     GPIO.output(GREEN_LED, False)
-    weekday = now.weekday() # trash only picked up on Thu or Fri
+    weekday = now.weekday() # Thu or Fri are only trash days
     if weekday == 3: # Thu
         if not is_holiday(now):
             leds_on(is_recycle_week(now))
     elif weekday == 4: # Fri
-        if is_holiday(now + timedelta(days = - 1)):
-            leds_on(is_recycle_week(now - 1))
+        previous_day = now + timedelta(days = - 1)
+        if is_holiday(previous_day):
+            leds_on(previous_day)
 
 def update_time(now):
-    display.colon = now.second % 2 # blink colon on even seconds
+    display.colon = now.second % 2 # colon on even seconds
     display.print(now.strftime("%H%M"))
 
-print('Press Ctrl-C to quit.')
+print('Trash Day Clock\n\nPress Ctrl-C to quit...')
 
 try:
     while(True):
